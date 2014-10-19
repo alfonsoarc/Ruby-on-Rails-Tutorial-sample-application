ffe92cb667459dd3f2b91898db39f36743720f47
alfonsoarc <alfonsoarc@hotmail.com>
2014-10-19 11:53:19 +0200

    7.3 Signup failure and fixes for change in tutorial version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
   def show
     @user = User.find(params[:id])
     #debugger
   end
+  
+  
+  def new
+    @user = User.new
+  end
+  # From new form, post to /users that goes to create action
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
