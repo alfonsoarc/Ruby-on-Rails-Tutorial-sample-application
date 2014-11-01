eee76002b3bdf9e32df1b7a29c7b523943c43891
alfonsoarc <alfonsoarc@hotmail.com>
2014-11-01 11:25:55 +0100

    8.2 Signin success

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,15 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+          it { should have_title(full_title(user.name)) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          # test the signout link to verify that the user was successfully signed in after signing up.
+          it { should have_link('Sign out', href: signout_path) }
+      end
     end
   end
 end
